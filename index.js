c27e7d42cce5c802a219514b321608620bb08895
idealmoon bby <idealmoonbby@idealmoons-MBP.station>
2023-03-19 20:38:23 +0100

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-//let x = prompt("Ваше імʼя?");
-//alert("Hello," + x + "! How are you?");
-
-let firstString = "5";
-let c = "7";
-let z = firstString + c;
-console.log(z);
+let x = prompt("Ваше імʼя?");
+alert("Hello," + x + "! How are you?");
